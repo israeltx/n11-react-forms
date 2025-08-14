95916347f006d328e65fbc7a36818523f44bc74d
Israel Teixeira <israeltx@gmail.com>
2025-08-14 14:19:05 -0300

    Implement state change from input's value

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react"
 import "./App.css"
 
 export default function App() {
+  const [name, setName] = useState('')
+
   return (
     <div>
-      <h1>Evento</h1>
+      <h1>Evento {name}</h1>
 
       <form>
-        <input type="text" placeholder="Nome do evento" />
+        <input type="text" placeholder="Nome do evento" onChange={(e) => setName(e.target.value)}/>
         <span className="error">Nome é obrigatório</span>
 
         <input type="date" placeholder="Nome do evento" lang="pt-BR" />
