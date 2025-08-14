348ed82ceeb38dffe35940bf8899e37aa687f1b8
Israel Teixeira <israeltx@gmail.com>
2025-08-14 16:43:26 -0300

    Exibit validation errors

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const schema = yup.object({
 })
 
 export default function App() {
-  const {control, handleSubmit} = useForm<FormData>({
+  const {control, handleSubmit, formState: {errors}} = useForm<FormData>({
     defaultValues: {
       name: '',
       date: '',
@@ -46,7 +46,7 @@ export default function App() {
           )}
         />
 
-        <span className="error">Nome é obrigatório</span>
+        {errors.name?.message && <span className="error">{errors.name.message}</span>}
 
         <Controller 
           control={control}
@@ -54,8 +54,10 @@ export default function App() {
           render={({field}) => (
             <input type="date" placeholder="Nome do evento" lang="pt-BR" {...field}/>
           )}
-        />        
+        />
         
+        {errors.date?.message && <span className="error">{errors.date.message}</span>}
+                
         <Controller 
           control={control}
           name="subject"
@@ -71,7 +73,9 @@ export default function App() {
               <option value="typescript">Typescript</option>
             </select>
           )}
-        />        
+        />
+
+        {errors.subject?.message && <span className="error">{errors.subject.message}</span>}
 
         <Controller 
           control={control}
@@ -79,7 +83,9 @@ export default function App() {
           render={({field}) => (
             <textarea placeholder="Descrição" rows={4}  {...field}/>
           )}
-        />  
+        />
+
+        {errors.description?.message && <span className="error">{errors.description.message}</span>}
 
         <button type="submit">Salvar</button>
 
